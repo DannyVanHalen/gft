0d616dd3a65b202fefcf29cbdca145231d6ea5b1
DannyVanHalen <danny.van.halen.87@gmail.com>
2018-03-10 16:46:28 +0100

    danny-develop: commit proyecto inicial 10/3/2018

---
+++ gtf-project/src/main/java/com/dfa/gtf/GtfProjectApplication.java
@@ -0,0 +1,12 @@
+package com.dfa.gtf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GtfProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GtfProjectApplication.class, args);
+	}
+}
